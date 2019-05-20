92099b4be7549b86c1556cc5c26afe3069b85ec7
Kim Jun Sik <jinaganda5@gmail.com>
2019-05-20 10:26:03 +0900

    Brush Size

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d");
 const colors = document.getElementsByClassName("jsColor");
+const range = document.getElementById("jsRange");
+const mode = document.getElementById("jsMode");
 
 canvas.width = 700;
 canvas.height = 700;
@@ -9,6 +11,7 @@ ctx.strokeStyle = "#2c2c2c";
 ctx.lineWidth = 2.5;
 
 let painting = false;
+let filling = false;
 
 function startPainting() {
   painting = true;
@@ -35,6 +38,21 @@ function handleColorClick(event) {
   ctx.strokeStyle = color;
 }
 
+function handleRangeBar(event) {
+  const width = event.target.value;
+  ctx.lineWidth = width;
+}
+
+function handleMode() {
+  if (filling === true) {
+    filling = false;
+    mode.innerText = "FILL";
+  } else {
+    filling = true;
+    mode.innerText = "PAINT";
+  }
+}
+
 if (canvas) {
   canvas.addEventListener("mousemove", onMouseMove);
   canvas.addEventListener("mousedown", startPainting);
@@ -45,3 +63,11 @@ if (canvas) {
 Array.from(colors).forEach(apple =>
   apple.addEventListener("click", handleColorClick)
 );
+
+if (range) {
+  range.addEventListener("input", handleRangeBar);
+}
+
+if (mode) {
+  mode.addEventListener("click", handleMode);
+}
